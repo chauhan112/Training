fecd3069ebcb67bf94919d35b3e1e0b9d41ea2e6
ha981muk-git <64093086+ha981muk-git@users.noreply.github.com>
2022-03-18 14:32:59 +0100

    few changes were made

---
+++ harsh/prime_numbers.py
@@ -0,0 +1,16 @@
+def prime_number_check(n):               # function for prime numbers
+    if n > 1:
+        for i in range(2, int(n / 2)):   # 2 is minimum and n/2 is the highest divider for a integer number
+            if n % i == 0:
+                return False             # in case if the number is divisible with any number it will return False.
+        return True                      # if not divisible with any number return True therefore it is prime number.
+
+
+print((prime_number_check(3)))
+print(prime_number_check(33))
+print(prime_number_check(17))
+print(prime_number_check(34))
+print(prime_number_check(45))
+print(prime_number_check(89))
+print(prime_number_check(101))
+print(prime_number_check(5))
