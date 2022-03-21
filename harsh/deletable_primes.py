07fc59d1ecbe6960d3a1849e6c9852467af48fdb
ha981muk-git <64093086+ha981muk-git@users.noreply.github.com>
2022-03-21 10:05:33 +0100

    few changes were made

--- harsh/deletable_primes.py
+++ harsh/deletable_primes.py
@@ -1,9 +1,26 @@
 def prime_number_check(n):  # function for prime numbers
     if n > 1:
-        for i in range(2, int(n / 2)):  # 2 is minimum and n/2 is the highest divider for a integer number
+        for i in range(2, int(n / 2)+1):  # 2 is minimum and n/2 is the highest divider for a integer number
             if n % i == 0:                #
                 return False  # in case if the number is divisible with any number it will return False.
         return True  # if not divisible with any number return True therefore it is prime number.
 
 def deletable_prime(n):
-    
+    if prime_number_check(n):       # First it should be checked if it is prime or not prime
+        arr  = []
+        while(n<=9):
+           a =  n%10
+           n = (n-a)/10
+           arr.append(a)
+
+         b = len(arr)
+         for i in arr:
+             
+
+
+
+
+
+
+def number_of_ways(n):
+
